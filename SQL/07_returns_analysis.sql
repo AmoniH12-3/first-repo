7461b343042c9d00db08e589ef446bc3582ae9ae
Amoni Haynes <amonihaynes@gmail.com>
2025-04-12 15:32:45 -0400

    Products with Most Returns

---
+++ SQL/07_returns_analysis.sql
@@ -0,0 +1,9 @@
+-- STEP 6: Products with Most Returns
+
+SELECT 
+    Description,
+    SUM(Quantity) AS total_returned
+FROM `subtle-signal-456022-n0.E_Commerce_Data.E_Commerce_Sales_Data`
+WHERE Quantity < 0
+GROUP BY Description
+ORDER BY total_returned;
