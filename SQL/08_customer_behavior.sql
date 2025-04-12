c74b01a4a5439f45b69e9f4de29932d57edb1233
Amoni Haynes <amonihaynes@gmail.com>
2025-04-12 15:34:34 -0400

    Customer Purchase Behavior & Average order value

---
+++ SQL/08_customer_behavior.sql
@@ -0,0 +1,11 @@
+-- STEP 7: Customer Purchase Behavior & AOV
+
+SELECT 
+    CustomerID,
+    COUNT(DISTINCT InvoiceNo) AS total_orders,
+    SUM(Quantity * UnitPrice) AS total_spent,
+    SUM(Quantity * UnitPrice)/COUNT(DISTINCT InvoiceNo) AS avg_order_value
+FROM `subtle-signal-456022-n0.E_Commerce_Data.E_Commerce_Sales_Data`
+GROUP BY CustomerID
+ORDER BY avg_order_value DESC
+LIMIT 10;
