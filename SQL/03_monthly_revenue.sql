9eb00b90e5973ed9a070d5862c61931f3eed1970
Amoni Haynes <amonihaynes@gmail.com>
2025-04-12 15:10:06 -0400

    Analyzes 	Revenue trends by month

--- SQL/03_monthly_revenue.sql
+++ SQL/03_monthly_revenue.sql
@@ -1,1 +1,8 @@
+-- STEP 2: Monthly Revenue Trend
 
+SELECT 
+    DATE_TRUNC(InvoiceDate, MONTH) AS month,
+    SUM(Quantity * UnitPrice) AS monthly_revenue
+FROM `subtle-signal-456022-n0.E_Commerce_Data.E_Commerce_Sales_Data`
+GROUP BY month
+ORDER BY month;
