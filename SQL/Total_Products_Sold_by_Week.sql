b7cba2cb75ee6aaa7b8d9362b6a5858538037b2e
Amoni Haynes <amonihaynes@gmail.com>
2025-04-15 23:24:27 -0400

    Add Total_Products_Sold_by_Week

---
+++ SQL/Total_Products_Sold_by_Week.sql
@@ -0,0 +1,6 @@
+### Showcases the total number of products sold by each week within the time period
+SELECT
+`Week Ending`,
+`Total Products Sold`
+FROM `sylvan-shuttle-456019-r2.Cannabis_Retail_Sales.Cannabis Retail Sales Data`
+ORDER BY `Week Ending`;
